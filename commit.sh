4b3d3aec2a036e53f620b9f8df8d1018420a8fa7
sathya111 <sathya5838@gmail.com>
2023-08-29 18:30:49 +0530

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -8,19 +8,4 @@ rm -rf .git
 
 cd ..
 
-#GIT_CMD_REPOSITORY="https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/TestGitSync/_git/TestGitSync"
-GIT_CMD_REPOSITORY="https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG//Dot-Net-Demo/_git/arm
 
-git clone $GIT_CMD_REPOSITORY
-
-cp -r testAzureDevops/* Dot-Net-Demo/
-
-cd Dot-Net-Demo
-
-git config --global user.email "$AZUSER_EMAIL"
-git config --global user.name "$AZUSERNAME"
-
-git add .
-git commit -m "sync from git to azure"
-
-git push
