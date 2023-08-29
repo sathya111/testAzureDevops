650d66c3f50a09a97d767ad245bcdbe7908b683d
sathya111 <sathya5838@gmail.com>
2023-08-30 00:59:05 +0530

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -41,4 +41,5 @@ git commit -m "sync from git to azure"
 git status
 
 #git push https://sathyapinreddy@dev.azure.com/sathyapinreddy/Dot-Net-Demo/_git/arm
-git push origin https://$AZUREPAT@dev.azure.com/$AZORG//_git/arm
+git remote set-url origin https://sathyapinreddy@dev.azure.com/sathyapinreddy/Dot-Net-Demo/_git/arm
+git push
