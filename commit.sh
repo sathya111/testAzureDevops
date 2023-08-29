c6e62ddb309c74c91fd5557fe216fbae1c6e1d5e
sathya111 <sathya5838@gmail.com>
2023-08-29 18:10:04 +0530

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -2,18 +2,20 @@ AZUREPAT=$AZUREPAT
 AZUSERNAME=$AZUSERNAME
 AZUSER_EMAIL=$AZUSER_EMAIL
 AZORG=$AZORG
-git clone https://github.com/harishlalwani/testAzureDevops
+git clone https://github.com/sathya111/testAzureDevops
 cd testAzureDevops
 rm -rf .git
 
 cd ..
 
-GIT_CMD_REPOSITORY="https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/TestGitSync/_git/TestGitSync"
+#GIT_CMD_REPOSITORY="https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/TestGitSync/_git/TestGitSync"
+GIT_CMD_REPOSITORY="https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG//Dot-Net-Demo/_git/arm
+
 git clone $GIT_CMD_REPOSITORY
 
-cp -r testAzureDevops/* TestGitSync/
+cp -r testAzureDevops/* Dot-Net-Demo/
 
-cd TestGitSync
+cd Dot-Net-Demo
 
 git config --global user.email "$AZUSER_EMAIL"
 git config --global user.name "$AZUSERNAME"
