675d5d2a34899e0a31de8ef9e6c197aad9409492
sathya111 <sathya5838@gmail.com>
2023-08-29 19:57:20 +0530

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -3,23 +3,40 @@ AZUSERNAME=$AZUSERNAME
 AZUSER_EMAIL=$AZUSER_EMAIL
 AZORG=$AZORG
 git clone https://github.com/sathya111/testAzureDevops
+####################
+###################list files and driectories #############
+ls
+
+
+################change directory###############
 cd testAzureDevops
+#######display files############
+ls 
 rm -rf .git
-
+############change directory #########
 cd ..
-
+ls 
+################
 GIT_CMD_REPOSITORY="https://dev.azure.com/sathyapinreddy/Dot-Net-Demo/_git/arm"
-
+################
 git clone $GIT_CMD_REPOSITORY
+################list the azure repos directory ##############
 ls -ltr
 cp -r testAzureDevops/* arm/
 
+######change the directory to arm azure repos path##########
 cd arm
-ls
+##########################
+######show files of github files copied to azure rpos###########
 git config --global user.email "$AZUSER_EMAIL"
+git config --global user.email
 git config --global user.name "$AZUSERNAME"
+git config --global user.name
 
+git status
 git add .
+git status
 git commit -m "sync from git to azure"
+git status
 
 git push
